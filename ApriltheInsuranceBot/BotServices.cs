f35f436ab9304cb5d619c0e52e8aef42e899ed57
sushma_rao <raosr@umich.edu>
2019-01-29 02:06:52 -0500

    Re-aligned BotServices.cs to root

--- ApriltheInsuranceBot/BotServices.cs
+++ ApriltheInsuranceBot/BotServices.cs
@@ -1,12 +1,11 @@
-﻿using System;
+﻿using Microsoft.Bot.Builder.AI.Luis;
+using Microsoft.Bot.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.Bot.Builder.AI;
-using Microsoft.Bot.Builder.AI.Luis;
-using Microsoft.Bot.Configuration;
 
-namespace ApriltheInsuranceBot.NewBot
+namespace ApriltheInsuranceBot
 {
     public class BotServices
     {
